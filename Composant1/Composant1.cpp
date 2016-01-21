fc56fea1dddf6be9b86c937c3ded74014d202285
jha2015 <jhaellemouzita@gmail.com>
2016-01-21 18:19:50 +0100

    new composant3

--- Composant1/Composant1.cpp
+++ Composant1/Composant1.cpp
@@ -1,7 +1,8 @@
 
 #include "Composant1.h"
 #include "Composant1Version.h"
-
+#include "Composant3.h"
+#include "Composant3Version.h"
 // declaration des fonctions internes
 int addition_interne(int a1, int a2);
 
@@ -9,7 +10,10 @@ int addition_interne(int a1, int a2);
 
 int composant1(int p1, int p2)
 {
-	return addition_interne(p1,p2);
+	
+	int valeur=composant2(p1,p2);
+	//return addition_interne(p1,p2);
+	return valeur;
 }
 
 int addition_interne(int a1, int a2)
